2ccf7119261fe71eb419cc0fa6f3181245408c61
fiske92 <fiske92@live.com>
2024-09-28 20:50:51 +0200

    Style changes

--- single-project.php
+++ single-project.php
@@ -2,11 +2,6 @@
 
 <main class="w-100">
   <article>
-    <h1 class="pt-3 pt-md-5"><?php the_title(); ?></h1>
-    <div class="bezimeni-single-project-separator bg-primary"></div>
-
-    <?php the_post_thumbnail('full', ['class' => 'w-100 pb-3 pb-md-5']); ?>
-
     <?php the_content(); ?>
   </article>
 </main>
